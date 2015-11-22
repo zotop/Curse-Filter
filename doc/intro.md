da320dd826c9d8b59d8e9cdb8d142685d1dc78dd
Joao Guerreiro <jmdguerreiro@gmail.com>
2015-11-22 19:34:00 +0100

    initial lein folder commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to curse-filter
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/curse_filter/core_test.clj
@@ -0,0 +1,7 @@
+(ns curse-filter.core-test
+  (:require [clojure.test :refer :all]
+            [curse-filter.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
